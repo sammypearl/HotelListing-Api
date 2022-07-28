9dce9422584e7174f3c18fce1b5feef820f7685a
Sammy Aman Akinsoju <sammypearls87@gmail.com>
2022-07-28 15:32:11 +0100

    recommit

---
+++ HotelListing Api/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace HotelListing_Api
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        }
+    }
+}
